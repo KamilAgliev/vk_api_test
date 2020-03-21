9e5d378aab32f69c088145dded664a321783956f
Без разницы <k.agliev@mail.ru>
2020-03-21 20:00:04 +0300

    10 task

--- testing_vk_api.py
+++ testing_vk_api.py
@@ -19,36 +19,39 @@ def index(group_id):
                               token=GROUP_TOKEN)
     vk_session.auth()
     vk = vk_session.get_api()
-    response = vk.stats.get(group_id=group_id, intervals_count=10, access_key=GROUP_TOKEN)
     data = {}
     data['message'] = "access denied!"
-    if response:
-        data['message'] = 'success'
-        data['likes'] = 0
-        data['comments'] = 0
-        data['subscribed'] = 0
-        data['12-18'] = 0
-        data['18-21'] = 0
-        data['21-24'] = 0
-        data['24-27'] = 0
-        data['27-30'] = 0
-        data['30-35'] = 0
-        data['35-45'] = 0
-        data['45-100'] = 0
-        data['cities'] = set()
+    try:
+        response = vk.stats.get(group_id=group_id, intervals_count=10, access_key=GROUP_TOKEN)
+        if response:
+            data['message'] = 'success'
+            data['likes'] = 0
+            data['comments'] = 0
+            data['subscribed'] = 0
+            data['12-18'] = 0
+            data['18-21'] = 0
+            data['21-24'] = 0
+            data['24-27'] = 0
+            data['27-30'] = 0
+            data['30-35'] = 0
+            data['35-45'] = 0
+            data['45-100'] = 0
+            data['cities'] = set()
 
-        for item in response:
-            try:
-                data['likes'] += item['activity']['likes']
-                data['comments'] += item['activity']['comments']
-                data['subscribed'] += item['activity']['subscribed']
-                for age in item['reach']['age']:
-                    data[age['value']] += age['count']
-                for city in item['reach']['cities']:
-                    data['cities'].add(city['name'])
-            except Exception:
-                pass
-        data['cities'] = list(data['cities'])
+            for item in response:
+                try:
+                    data['likes'] += item['activity']['likes']
+                    data['comments'] += item['activity']['comments']
+                    data['subscribed'] += item['activity']['subscribed']
+                    for age in item['reach']['age']:
+                        data[age['value']] += age['count']
+                    for city in item['reach']['cities']:
+                        data['cities'].add(city['name'])
+                except Exception:
+                    pass
+            data['cities'] = list(data['cities'])
+    except Exception:
+        pass
     return render_template('stats.html', data=data)
 
 
